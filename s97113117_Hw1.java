a1d72a6c5df32328efda6affffe752bd40a4b4f5
david <s97113117.stu@gmail.com>
2011-10-05 22:58:30 +0800

    新增主類別, 宣告學號、重建資料等必要資料

--- s97113117_Hw1.java
+++ s97113117_Hw1.java
@@ -2,3 +2,14 @@ import static java.lang.System.out;
 import static java.lang.Math.pow;
 import static java.lang.Math.log10;
 import java.text.DecimalFormat;
+
+public class s97113117_Hw1 {
+	public static void main(String[] args) {
+
+		int[] xn = {9, 7, 1, 1, 3, 1, 1, 7};	// 學號 (xn)
+		int[] yn = {1, 2, 3, 4, 5, 6, 7, 8};	// 重建資料 (yn)
+		int N = xn.length;	// 資料長度 (N)
+		int peak = 9;
+
+	}
+}
