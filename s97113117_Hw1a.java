f6f93eef1bf72edb26cdad06423c3ced9ab57eef
david <s97113117.stu@gmail.com>
2011-10-10 03:37:10 +0800

    add main

--- s97113117_Hw1a.java
+++ s97113117_Hw1a.java
@@ -4,3 +4,10 @@ import static java.lang.Math.PI;
 import static java.lang.Math.round;
 import java.text.DecimalFormat;
 
+public class s97113117_Hw1a {
+	public static void main(String[] args) {
+		// 範例
+		// int[] fi = {8, 7, 6, 5, 4, 3, 2, 1};
+		int[] fi = {9, 7, 1, 1, 3, 1, 1, 7};
+	}
+}
