fb31dd5b722063603d40290b382ad612d25eebc3
david <s97113117.stu@gmail.com>
2011-10-10 03:39:20 +0800

    add method to calculate for 1D DCT

--- s97113117_Hw1a.java
+++ s97113117_Hw1a.java
@@ -13,6 +13,24 @@ class TransformCoding {
 		this.fi = fi;
 		get1DDCT();
 	}
+
+	public double[] get1DDCT() {
+		double[] dct = new double[fi.length];
+		double cu;
+		double PI = Math.PI;
+
+		for (int u = 0; u < dct.length; u++) {
+			for (int i = 0; i < dct.length; i++) {
+				dct[u] += cos(((2 * i + 1) * u * PI) / 16) * fi[i];
+			}
+			// 保留小數後四位
+			dct[u] = round(dct[u] * 10000) / 10000.0;
+			// 判斷C(u)
+			cu = (0 == u) ? 0.35355339059 : 0.5;
+			dct[u] *= cu;
+		}
+		return dct;
+	}
 }
 
 public class s97113117_Hw1a {
@@ -21,5 +39,7 @@ public class s97113117_Hw1a {
 		// int[] fi = {8, 7, 6, 5, 4, 3, 2, 1};
 		int[] fi = {9, 7, 1, 1, 3, 1, 1, 7};
 		TransformCoding tc = new TransformCoding(fi);
+
+		double[] dct = tc.get1DDCT();
 	}
 }
